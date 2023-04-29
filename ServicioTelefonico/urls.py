e22a529ff5e36700181760e5b40af0342d59ac68
1drakr <frapazh@upt.pe>
2023-04-29 02:30:46 -0500

    apiserviciotelefonico_v1

--- ServicioTelefonico/urls.py
+++ ServicioTelefonico/urls.py
@@ -18,10 +18,31 @@ from django.contrib import admin
 from django.urls import include, path
 from post.api.router import router_posts
 from post.api.views import ClienteViewSet
+from post.api.views import crear_plan_con_factura
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Servicio Telefonico Movil",
+      default_version='v1',
+      description="Api para un servicion de telefonia movil en un entidad bancaria",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+#    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router_posts.urls)),
     path('clientes/<int:pk>/cancelar_servicio/', ClienteViewSet.as_view({'put': 'cancelar_servicio'})),
+    path('planes/crear-con-factura/', crear_plan_con_factura, name='crear_plan_con_factura'),
+    path(r'docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(r'redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 
 ]
