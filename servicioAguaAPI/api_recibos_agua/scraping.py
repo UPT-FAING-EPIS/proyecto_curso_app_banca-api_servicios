c90f993097fcbd6127655bead078756f49cc1a0e
Sebastian Cotrina <102818449+SebastianCotrina16@users.noreply.github.com>
2023-06-03 07:07:49 -0700

    Update scraping.py

--- servicioAguaAPI/api_recibos_agua/scraping.py
+++ servicioAguaAPI/api_recibos_agua/scraping.py
@@ -3,33 +3,33 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-def obtener_deuda(url, codigo_cliente, correo, dni):
-    payload = {
-        'codigo': codigo_cliente,
-        'email': correo,
-        'dni': dni,
-    }
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
 
-    response = requests.post(url, data=payload)
-    soup = BeautifulSoup(response.content, "html.parser")
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
 
-    total_a_pagar_label = soup.find("th", string="TOTAL A PAGAR: ")
-    total_a_pagar_value = None
-    if total_a_pagar_label:
-        total_a_pagar_value = total_a_pagar_label.find_next_sibling("th").text.strip()
+class WebScraper(metaclass=Singleton):
+    def __init__(self):
+        self.session = requests.Session()
 
-    return total_a_pagar_value
-'''
-url = "https://pagovisa.epstacna.com.pe:8443" 
-codigo_cliente = "11111"  
-correo = "apiagua@gmail.com"
-dni = "87654321"
+    def obtener_deuda(self, url, codigo_cliente, correo, dni):
+        payload = {
+            'codigo': codigo_cliente,
+            'email': correo,
+            'dni': dni,
+        }
 
-deuda = obtener_deuda(url, codigo_cliente, correo, dni)
+        response = self.session.post(url, data=payload)
+        soup = BeautifulSoup(response.content, "html.parser")
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
 
-if deuda is not None:
-    print(f"Deuda para el cliente {codigo_cliente}: {deuda}")
-else:
-    print("No se encontró información de deuda para el cliente.") '''
+        total_a_pagar_label = soup.find("th", string="TOTAL A PAGAR: ")
+        total_a_pagar_value = None
+        if total_a_pagar_label:
+            total_a_pagar_value = total_a_pagar_label.find_next_sibling("th").text.strip()
+
+        return total_a_pagar_value
